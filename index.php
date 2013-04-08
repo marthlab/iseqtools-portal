fe3ad0155e83692e6b7a0ce41666449fa4c63b9a
Brian D'Astous <brian.dastous@gmail.com>
2013-04-08 17:39:05 -0400

    started dagre integration

--- index.php
+++ index.php
@@ -20,7 +20,23 @@
             <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
         <![endif]-->
 
-        <p>Hello world! This is HTML5 Boilerplate.</p>
+        <h2>Graph Visualization</h2>
+
+        <svg width=0 height=0>
+          <defs>
+            <marker id="arrowhead"
+                    viewBox="0 0 10 10"
+                    refX="8"
+                    refY="5"
+                    markerUnits="strokeWidth"
+                    markerWidth="8"
+                    markerHeight="5"
+                    orient="auto"
+                    style="fill: #333">
+              <path d="M 0 0 L 10 5 L 0 10 z"></path>
+            </marker>
+          </defs>
+        </svg>
 
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
--- js/main.js
+++ js/main.js
@@ -45,8 +45,8 @@
 	    }, this).toDict();
 
     	// backreferences populated later
-    	this.in_objectives = {};
-    	this.out_objectives = {};
+    	this.objectives_yielding = {};
+    	this.objectives_accepting = {};
     }
 
     function Tool(config, objective) {
@@ -54,11 +54,11 @@
     	this.order = config.order;
     	this.name = config.name;
     	this.objective = objective;
-    	this.in_data_formats = _(app.data_formats).objFilter(function(df) {
-    		return _(config.in_data_formats).contains(df.id);
+    	this.data_formats_in = _(app.data_formats).objFilter(function(df) {
+    		return _(config.data_formats_in).contains(df.id);
     	});
-    	this.out_data_formats = _(app.data_formats).objFilter(function(df) {
-    		return _(config.out_data_formats).contains(df.id);
+    	this.data_formats_out = _(app.data_formats).objFilter(function(df) {
+    		return _(config.data_formats_out).contains(df.id);
     	});
 
     	// backreferences populated later
@@ -69,7 +69,7 @@
     	this.id = config.id;
     	this.order = config.order;
     	this.name = config.name;
-    	this.nontool_out_data_formats = _.objFilter(app.data_formats, function(df) {
+    	this.nontool_data_formats_out = _.objFilter(app.data_formats, function(df) {
     		return _(config.nontool_out_data_format_ids).contains(df.id);
     	});
 
@@ -78,10 +78,10 @@
 	    }, this).toDict();
 
     	// convenience properties
-    	this.in_data_formats = _.extend.apply({}, _(this.tools).map(function(tool) {return tool.in_data_formats;}));
-    	this.in_data_types = _(this.in_data_formats).map(function(df) {return df.data_type;}).toDict();
-    	this.out_data_formats = _.extend.apply({}, _(this.tools).map(function(tool) {return tool.out_data_formats;}));
-    	this.out_data_types = _(this.out_data_formats).map(function(df) {return df.data_type;}).toDict();
+    	this.data_formats_in = _.extend.apply({}, _(this.tools).map(function(tool) {return tool.data_formats_in;}));
+    	this.data_types_in = _(this.data_formats_in).map(function(df) {return df.data_type;}).toDict();
+    	this.data_formats_out = _.extend.apply({}, _(this.tools).map(function(tool) {return tool.data_formats_out;}));
+    	this.data_types_out = _(this.data_formats_out).map(function(df) {return df.data_type;}).toDict();
     }
 
     function Workflow(config) {
@@ -138,12 +138,193 @@
   	});
 
   	_(app.objectives).each(function(obj) {
-    	_(obj.in_data_types).each(function(dt) {
-  			dt.in_objectives[obj.id] = obj;
+    	_(obj.data_types_in).each(function(dt) {
+  			dt.objectives_accepting[obj.id] = obj;
   		});
-  		_(obj.out_data_types).each(function(dt) {
-  			dt.out_objectives[obj.id] = obj;
+  		_(obj.data_types_out).each(function(dt) {
+  			dt.objectives_yielding[obj.id] = obj;
   		});
   	});
+
+  	// add more properties and data structures for dagre rendering
+
+  	function renderGlobal() {
+  		var transitions_from_objectives = _(_(app.objectives).map(function(obj){
+  			return _(obj.data_types_out).map(function(dt) { 
+  				return {source: obj, target: dt};
+  			});
+  		})).flatten();
+
+  		var transitions_from_data_types = _(_(app.data_types).map(function(dt){
+  			return _(dt.objectives_accepting).map(function(obj) { 
+  				return {source: dt, target: obj};
+  			});
+  		})).flatten();
+
+  		var transitions = _.union(transitions_from_objectives, transitions_from_data_types);
+  		
+  	}
+
+  	  function spline(e) {
+		    var points = e.dagre.points.slice(0);
+		    var source = dagre.util.intersectRect(e.source.dagre, points[0]);
+		    var target = dagre.util.intersectRect(e.target.dagre, points[points.length - 1]);
+		    points.unshift(source);
+		    points.push(target);
+		    return d3.svg.line()
+		      .x(function(d) { return d.x; })
+		      .y(function(d) { return d.y; })
+		      .interpolate("linear")
+		      (points);
+		  }
+
+		  // Translates all points in the edge using `dx` and `dy`.
+		  function translateEdge(e, dx, dy) {
+		    e.dagre.points.forEach(function(p) {
+		      p.x = Math.max(0, Math.min(svgBBox.width, p.x + dx));
+		      p.y = Math.max(0, Math.min(svgBBox.height, p.y + dy));
+		    });
+		  }
+
+		  renderGlobal();
+
+		  // Get the data in the right form
+		  var stateKeys = {};
+		  transitions.forEach(function(d) {
+		    var source = stateKeys[d.source],
+		        target = stateKeys[d.target];
+		    if (!source) source = stateKeys[d.source] = { label: d.source, edges: [] };
+		    if (!target) target = stateKeys[d.target] = { label: d.target, edges: [] };
+		    source.edges.push(d);
+		    target.edges.push(d);
+		  });
+		  var states = d3.values(stateKeys);
+		  transitions.forEach(function(d) {
+		    d.source = stateKeys[d.source];
+		    d.target = stateKeys[d.target];
+		  });
+
+		  // Now start laying things out
+		  var svg = d3.select("svg");
+		  var svgGroup = svg.append("g").attr("transform", "translate(5, 5)");
+
+		  // `nodes` is center positioned for easy layout later
+		  var nodes = svgGroup
+		    .selectAll("g .node")
+		    .data(states)
+		    .enter()
+		      .append("g")
+		      .attr("class", "node")
+		      .attr("id", function(d) { return "node-" + d.label });
+
+		  var edges = svgGroup
+		    .selectAll("path .edge")
+		    .data(transitions)
+		    .enter()
+		      .append("path")
+		      .attr("class", "edge")
+		      .attr("marker-end", "url(#arrowhead)");
+
+		  // Append rectangles to the nodes. We do this before laying out the text
+		  // because we want the text above the rectangle.
+		  var rects = nodes.append("rect");
+
+		  // Append text
+		  var labels = nodes
+		    .append("text")
+		      .attr("text-anchor", "middle")
+		      .attr("x", 0);
+
+		  labels
+		    .append("tspan")
+		    .attr("x", 0)
+		    .attr("dy", "1em")
+		    .text(function(d) { return d.label; });
+
+		  // We need width and height for layout.
+		  labels.each(function(d) {
+		    var bbox = this.getBBox();
+		    d.bbox = bbox;
+		    d.width = bbox.width + 2 * nodePadding;
+		    d.height = bbox.height + 2 * nodePadding;
+		  });
+
+		  rects
+		    .attr("x", function(d) { return -(d.bbox.width / 2 + nodePadding); })
+		    .attr("y", function(d) { return -(d.bbox.height / 2 + nodePadding); })
+		    .attr("width", function(d) { return d.width; })
+		    .attr("height", function(d) { return d.height; });
+
+		  labels
+		    .attr("x", function(d) { return -d.bbox.width / 2; })
+		    .attr("y", function(d) { return -d.bbox.height / 2; });
+
+		  // Create the layout and get the graph
+		  dagre.layout()
+		    .nodeSep(50)
+		    .edgeSep(10)
+		    .rankSep(50)
+		    .nodes(states)
+		    .edges(transitions)
+		    .debugLevel(1)
+		    .run();
+
+		  nodes.attr("transform", function(d) { return 'translate('+ d.dagre.x +','+ d.dagre.y +')'; });
+
+		  // Ensure that we have at least two points between source and target
+		  edges.each(function(d) {
+		    var points = d.dagre.points;
+		    if (!points.length) {
+		      var s = d.source.dagre;
+		      var t = d.target.dagre;
+		      points.push({ x: (s.x + t.x) / 2, y: (s.y + t.y) / 2 });
+		    }
+
+		    if (points.length === 1) {
+		      points.push({ x: points[0].x, y: points[0].y });
+		    }
+		  });
+
+		  edges
+		    // Set the id. of the SVG element to have access to it later
+		    .attr('id', function(e) { return e.dagre.id; })
+		    .attr("d", function(e) { return spline(e); });
+
+		  // Resize the SVG element
+		  var svgBBox = svg.node().getBBox();
+		  svg.attr("width", svgBBox.width + 10);
+		  svg.attr("height", svgBBox.height + 10);
+
+		  // Drag handlers
+		  var nodeDrag = d3.behavior.drag()
+		    // Set the right origin (based on the Dagre layout or the current position)
+		    .origin(function(d) { return d.pos ? {x: d.pos.x, y: d.pos.y} : {x: d.dagre.x, y: d.dagre.y}; })
+		    .on('drag', function (d, i) {
+		      var prevX = d.dagre.x,
+		          prevY = d.dagre.y;
+
+		      // The node must be inside the SVG area
+		      d.dagre.x = Math.max(d.width / 2, Math.min(svgBBox.width - d.width / 2, d3.event.x));
+		      d.dagre.y = Math.max(d.height / 2, Math.min(svgBBox.height - d.height / 2, d3.event.y));
+		      d3.select(this).attr('transform', 'translate('+ d.dagre.x +','+ d.dagre.y +')');
+
+		      var dx = d.dagre.x - prevX,
+		          dy = d.dagre.y - prevY;
+
+		      // Edges position (inside SVG area)
+		      d.edges.forEach(function(e) {
+		        translateEdge(e, dx, dy);
+		        d3.select('#'+ e.dagre.id).attr('d', spline(e));
+		      });
+		    });
+
+		  var edgeDrag = d3.behavior.drag()
+		    .on('drag', function (d, i) {
+		      translateEdge(d, d3.event.dx, d3.event.dy);
+		      d3.select(this).attr('d', spline(d));
+		    });
+
+		  nodes.call(nodeDrag);
+		  edges.call(edgeDrag);
 
 // }());
