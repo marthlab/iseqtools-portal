0e1c2a2c036d807fd8dbac0dfaf9777fb1755462
Brian D'Astous <brian.dastous@gmail.com>
2013-04-08 16:16:05 -0400

    use dictionaries instead of arrays

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
 
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
-        <script src="js/vendor/sugar-1.3.9.min.js"></script>
+        <script src="http://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.4.4/underscore-min.js"></script>
         <script src="http://d3js.org/d3.v3.min.js"></script>
         <script src="js/vendor/dagre.js"></script>
 
--- js/main.js
+++ js/main.js
@@ -1,128 +1,149 @@
 
 //Immediately-Invoked Function Expression 
-(function() {
+// (function() {
 
-		function by_id(id) {
-			return (function(obj) {
-				return obj.hasOwnProperty('id') && obj.id == id;
-			});
-		}
+		_.mixin({
+		  // ### _.objFilter
+		  // _.filter for objects, keeps key/value associations
+		  // but only includes the properties that pass test().
+		  objFilter: function (input, test, context) {
+		    return _.reduce(input, function (obj, v, k) {
+		             if (test.call(context, v, k, input)) {
+		               obj[k] = v;
+		             }
+		             return obj;
+		           }, {}, context);
+		  }
+		});
 
-		Array.prototype.contains = function(obj) {
-			return this.indexOf(obj) > -1;
-		}
+	  // creates a dictionary from an array of objects. each object in
+	  // the array must have a unique value for key_property that is
+	  // a syntactically valid JS object key (defaults to "id")
+	  Array.prototype.toDict = function(key_property) {
+	  	new_obj = {};
+	  	key_property = key_property || "id";
+	  	_.each(this, function(element, index) {
+	  		new_obj[element[key_property]] = element;
+	  	});
+	  	return new_obj;
+	  }
 
     function DataFormat(config, data_type) {
     	this.id = config.id;
+    	this.order = config.order;
     	this.name = config.name;
     	this.data_type = data_type;
     }
 
     function DataType(config) {
     	this.id = config.id;
+    	this.order = config.order;
     	this.name = config.name;
 
-    	this.data_formats = config.data_formats.map(function(data_format_config) {
-    		return new DataFormat(data_format_config, this);
-    	}, this);
+	    this.data_formats = config.data_formats.map(function(df_cfg, order) {
+	    	return new DataFormat(_.extend(df_cfg, {order: order}), this);
+	    }, this).toDict();
 
     	// backreferences populated later
-    	this.in_objectives = [];
-    	this.out_objectives = [];
+    	this.in_objectives = {};
+    	this.out_objectives = {};
     }
 
     function Tool(config, objective) {
     	this.id = config.id;
+    	this.order = config.order;
     	this.name = config.name;
     	this.objective = objective;
-    	this.in_data_formats = app.data_formats.filter(function(df) {
-    		return config.in_data_formats.contains(df.id);
+    	this.in_data_formats = _(app.data_formats).objFilter(function(df) {
+    		return _(config.in_data_formats).contains(df.id);
     	});
-    	this.out_data_formats = app.data_formats.filter(function(df) {
-    		return config.out_data_formats.contains(df.id);
+    	this.out_data_formats = _(app.data_formats).objFilter(function(df) {
+    		return _(config.out_data_formats).contains(df.id);
     	});
 
     	// backreferences populated later
-    	this.pipelines = [];
+    	this.pipelines = {};
     }
 
     function Objective(config) {
     	this.id = config.id;
+    	this.order = config.order;
     	this.name = config.name;
-    	this.nontool_out_data_formats = app.data_formats.filter(function(df) {
-    		return config.nontool_out_data_format_ids.contains(df.id);
+    	this.nontool_out_data_formats = _.objFilter(app.data_formats, function(df) {
+    		return _(config.nontool_out_data_format_ids).contains(df.id);
     	});
 
-    	this.tools = config.tools.map(function(tool_config) {
-    		return new Tool(tool_config, this);
-    	}, this);
+	    this.tools = config.tools.map(function(tool_cfg, order) {
+	    	return new Tool(_.extend(tool_cfg, {order: order}), this);
+	    }, this).toDict();
 
     	// convenience properties
-    	this.in_data_formats = this.tools.map(function(tool) {return tool.in_data_formats;}).flatten().unique();
-    	this.in_data_types = this.in_data_formats.map(function(df) {return df.data_type;}).unique();
-    	this.out_data_formats = this.tools.map(function(tool) {return tool.out_data_formats;}).flatten().union(config.nontool_out_data_formats).unique();
-    	this.out_data_types = this.out_data_formats.map(function(df) {return df.data_type;}).unique();
-    	
+    	this.in_data_formats = _.extend.apply({}, _(this.tools).map(function(tool) {return tool.in_data_formats;}));
+    	this.in_data_types = _(this.in_data_formats).map(function(df) {return df.data_type;}).toDict();
+    	this.out_data_formats = _.extend.apply({}, _(this.tools).map(function(tool) {return tool.out_data_formats;}));
+    	this.out_data_types = _(this.out_data_formats).map(function(df) {return df.data_type;}).toDict();
     }
 
     function Workflow(config) {
     	this.id = config.id;
+    	this.order = config.order;
     	this.name = config.name;
 
-    	this.pipelines = config.pipelines.map(function(pipeline_config) {
-    		return new Pipeline(pipeline_config, this);
-    	}, this);
+    	this.pipelines = config.pipelines.map(function(pipeline_cfg, order) {
+	    	return new Pipeline(_.extend(pipeline_cfg, {order: order}), this);
+	    }, this).toDict();
 
     	// convenience properties
-    	this.tools = this.pipelines.map(function(pl) {return pl.tools;}).flatten().unique();
-    	this.objectives = this.pipelines.map(function(pl) {return pl.objectives;}).flatten().unique();
+    	this.tools = _.extend.apply({}, _(this.pipelines).map(function(pl) {return pl.tools;}));
+    	this.objectives = _.extend.apply({}, _(this.pipelines).map(function(pl) {return pl.objectives;}));
     }
 
     function Pipeline(config, workflow) {
     	this.id = config.id;
+    	this.order = config.order;
     	this.name = config.name;
     	this.workflow = workflow;
-    	this.tools = app.tools.filter(function(tool) {
-    		return config.tool_ids.contains(tool.id);
+    	this.tools = _(app.tools).objFilter(function(tool) {
+    		return _(config.tool_ids).contains(tool.id);
     	});
 
     	// convenience properties
-    	this.objectives = this.tools.map(function(tool) {return tool.objective;});
+    	this.objectives = _(this.tools).map(function(tool) {return tool.objective;}).toDict();
     }
 
     // global w.r.t. the containing IIFE
     var app = {};
 
     // add data in order of logical dependencies, and add convenience properties for nested objects
-    app.data_types = app_json.data_types.map(function(data_type_config) {
-  		return new DataType(data_type_config);
-  	});
-  	app.data_formats = app.data_types.map(function(dt){return dt.data_formats;}).flatten();
+    app.data_types = app_json.data_types.map(function(dt_cfg, order) {
+    	return new DataType(_.extend(dt_cfg, {order: order}) );
+    }).toDict();
+    app.data_formats = _.extend.apply({}, _(app.data_types).map(function(dt) {return dt.data_formats;}));
 
-  	app.objectives = app_json.objectives.map(function(objective_config) {
-  		return new Objective(objective_config);
-  	});
-  	app.tools = app.objectives.map(function(obj){return obj.tools;}).flatten();
+    app.objectives = app_json.objectives.map(function(obj_cfg, order) {
+    	return new Objective(_.extend(obj_cfg, {order: order}) );
+    }).toDict();
+    app.tools = _.extend.apply({}, _(app.objectives).map(function(obj) {return obj.tools;}));
 
-  	app.workflows = app_json.workflows.map(function(workflow_config) {
-  		return new Workflow(workflow_config);
-  	});
-  	app.pipelines = app.workflows.map(function(wf){return wf.pipelines;}).flatten();
-  	
+    app.workflows = app_json.workflows.map(function(wf_cfg, order) {
+    	return new Workflow(_.extend(wf_cfg, {order: order}) );
+    }).toDict();
+    app.pipelines = _.extend.apply({}, _(app.workflows).map(function(obj) {return obj.pipelines;}));
+
   	// add backreferences
-  	app.pipelines.forEach(function(pl) {
-    	pl.tools.forEach(function(tool) {
-  			tool.pipelines.push(pl);
+  	_(app.pipelines).each(function(pl) {
+    	_(pl.tools).each(function(tool) {
+  			tool.pipelines[pl.id] = pl;
   		});
   	});
 
-  	app.objectives.forEach(function(obj) {
-    	obj.in_data_types.forEach(function(dt) {
-  			dt.in_objectives.push(obj);
+  	_(app.objectives).each(function(obj) {
+    	_(obj.in_data_types).each(function(dt) {
+  			dt.in_objectives[obj.id] = obj;
   		});
-  		obj.out_data_types.forEach(function(dt) {
-  			dt.out_objectives.push(obj);
+  		_(obj.out_data_types).each(function(dt) {
+  			dt.out_objectives[obj.id] = obj;
   		});
   	});
 
-}());
+// }());
